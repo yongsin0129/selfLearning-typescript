7426ea714e273f8440dd069cd993af6b2e0cf8a4
yongsin0129 <yongsin0129@gmail.com>
2022-11-04 19:27:05 +0800

    add : note for Dead Zone

--- 01.basic/1.Inference_Anotation_TDZ_union.ts
+++ 01.basic/1.Inference_Anotation_TDZ_union.ts
@@ -9,6 +9,10 @@ let hasPet = false
 let nothing = undefined
 let nothingLiterally = null
 
+let a:String = new String()
+let b:Number = new Number()
+let c:Boolean = new Boolean()
+
 // 我們先定義好我們的變數，但是不要去帶入任何值就會變成 any
 let messageToSend // type : any
 // 帶入我們想要寫的值
@@ -26,15 +30,19 @@ let literallyAbsoluteNothing: null = null
 *
           Temporal Dead Zone，暫時性死區
           在理解 TDZ 時，必須了解 JS 的變數作用域以及變數提升的概念
+
+          在未確定變數被正式丟入合法的值之前的這段空間，不能使用該變數。
 *
 *********************************************************************************/
 let canBeNullableString: string
 // TDZ 暫時性死區的錯誤！
-// let myString = canBeNullableString // Variable 'canBeNullableString' is used before being assigned.ts(2454)
+let myString = canBeNullableString // Variable 'canBeNullableString' is used before being assigned.ts(2454)
 
 canBeNullableString = 'hello'
-// canBeNullableString = undefined // Type 'undefined' is not assignable to type 'string'.ts(2322)
-// canBeNullableString = null // Type 'null' is not assignable to type 'string'.ts(2322)
+let anotherString = canBeNullableString
+
+canBeNullableString = undefined // Type 'undefined' is not assignable to type 'string'.ts(2322)
+canBeNullableString = null // Type 'null' is not assignable to type 'string'.ts(2322)
 
 
 /********************************************************************************
